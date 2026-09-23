f49356d005fca986fd23f29d4c959949db523c05
Sivani <sivani@gmail.com>
2026-09-23 15:17:16 +0530

    Add employee form and display functionality

--- employee/views.py
+++ employee/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .forms import EmployeeForm
+from .models import Employee
 
-# Create your views here.
+
+def employee_form(request):
+
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+    else:
+        form = EmployeeForm()
+
+    employees = Employee.objects.all()
+
+    return render(
+        request,
+        'employee/employee_form.html',
+        {
+            'form': form,
+            'employees': employees
+        }
+    )
